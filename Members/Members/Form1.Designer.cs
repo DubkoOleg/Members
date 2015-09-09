98b2b687adec030ec5dfb3348bbc6d4fa900204e
DubkoOleg <dubkooleg@gmail.com>
2015-09-10 00:01:27 +0300

    Добавил чтение картинки

--- Members/Members/Form1.Designer.cs
+++ Members/Members/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(260, 237);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
+            this.Text = "Главная форма";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
     }
 }
 
--- Members/Members/Form1.cs
+++ Members/Members/Form1.cs
@@ -12,9 +12,25 @@ namespace Members
 {
     public partial class Form1 : Form
     {
+        private Bitmap MyImage;
+
         public Form1()
         {
             InitializeComponent();
+
+            var fileToDisplay = @"D:\Программмирование\GitHub\Members\Members\Members\Фото.jpg";
+
+            if (MyImage != null)
+            {
+                MyImage.Dispose();
+            }
+
+            // Stretches the image to fit the pictureBox.
+            pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            MyImage = new Bitmap(fileToDisplay);
+            MyImage.RotateFlip(RotateFlipType.Rotate270FlipNone);
+            //pictureBox.ClientSize = new Size(xSize, ySize);
+            pictureBox.Image = (Image)MyImage;
         }
     }
 }
